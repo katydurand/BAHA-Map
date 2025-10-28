5216b0ff592c31b4247141a9122394a0e8e19e29
katydurand <katydurand@berkeley.edu>
2025-10-28 15:27:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0eWR1cmFuZCIsImEiOiJjbWhiMW0zdXowMDZxMmpvb2U3ZGJmNW1kIn0.1SWX3PUdg5aLdTv6UhR_bg';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/katydurand/cmhb4cc4q000y01sm6r3w82sn',
+  center: [-122.27, 37.8],
+  zoom: 9
+});
